aef6f552836bddf13868a9acf8272569504dd9b2
Vova Pol <ya-chai777@yandex.ru>
2022-08-06 13:25:04 +0300

    fix: подключить стили

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ function popupClose() {
 }
 
 closeButton.addEventListener("click", popupClose);
+
+let profileName = page.querySelector(".profile__name");
+let profileOccupation = page.querySelector(".profile__occupation");
+
+let formElement = popup.querySelector(".popup__container");
+let nameInput = popup.querySelector(".popup__input-name");
+let occupationInput = popup.querySelector(".popup__input-occupation");
+
+nameInput.value = profileName.textContent;
+occupationInput.value = profileOccupation.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileOccupation.textContent = occupationInput.value;
+
+  popupClose();
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
