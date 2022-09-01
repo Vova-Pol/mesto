404fccea4028dab47164109f2b8493ce304dd9c8
Vova Pol <ya-chai777@yandex.ru>
2022-09-01 08:47:50 +0300

    refactor: Вынести код валидации форм в отдельный файл validate.js и подключить его в index.html

--- scripts/index.js
+++ scripts/index.js
@@ -172,65 +172,6 @@ popupImageCloseButton.addEventListener("click", () => {
   closePopup(popupImage);
 });
 
-/* ----- Edit Profile Form Validation ----- */
-
-function showInputError(inputElem, formElem) {
-  inputElem.classList.add("popup__input_type_error");
-  const errorElement = formElem.querySelector(`#${inputElem.id}-error`);
-  errorElement.textContent = inputElem.validationMessage;
-}
-
-function hideInputError(inputElem, formElem) {
-  inputElem.classList.remove("popup__input_type_error");
-  const errorElement = formElem.querySelector(`#${inputElem.id}-error`);
-  errorElement.textContent = "";
-}
-
-function isInputValid(inputElem, formElem) {
-  if (inputElem.validity.valid) {
-    hideInputError(inputElem, formElem);
-  } else {
-    showInputError(inputElem, formElem);
-  }
-}
-
-function setEventListenersForInputs(formElem) {
-  const inputsList = Array.from(formElem.querySelectorAll("input"));
-  const buttonElem = formElem.querySelector(".popup__save-button");
-
-  toggleSubmitButtonState(inputsList, buttonElem);
-
-  inputsList.forEach((inputElem) => {
-    inputElem.addEventListener("input", () => {
-      isInputValid(inputElem, formElem);
-      toggleSubmitButtonState(inputsList, buttonElem);
-    });
-  });
-}
-
-function validateAllForms() {
-  const formsList = Array.from(document.forms);
-  formsList.forEach((formElem) => {
-    setEventListenersForInputs(formElem);
-  });
-}
-
-validateAllForms();
-
-/* ----- Submit Button ----- */
-
-function hasInvalidInput(inputsList) {
-  return inputsList.some((input) => !input.validity.valid);
-}
-
-function toggleSubmitButtonState(inputsList, buttonElem) {
-  if (hasInvalidInput(inputsList)) {
-    buttonElem.setAttribute("disabled", "disabled");
-  } else {
-    buttonElem.removeAttribute("disabled");
-  }
-}
-
 /* ----- Overlay Close Popup ----- */
 
 const popupsList = Array.from(page.querySelectorAll(".popup"));
