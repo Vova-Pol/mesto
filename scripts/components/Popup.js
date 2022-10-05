7789580211f641350d03a67c069b65a97af2b05b
Vova Pol <ya-chai777@yandex.ru>
2022-10-05 11:50:17 +0300

    bugfix: исправлена функция закрытия попапов по нажатию Esc

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -3,21 +3,24 @@ export class Popup {
     this._element = document.querySelector(popupSelector);
     this._image = this._element.querySelector(".popup__image");
     this._subtitle = this._element.querySelector(".popup__subtitle");
+    this._handleEscButtonBound = this._handleEscClose.bind(this);
   }
 
   open() {
     this._element.classList.add("popup_opened");
-    document.addEventListener("keydown", this._handleEscClose);
+    document.addEventListener("keydown", this._handleEscButtonBound);
   }
 
   close() {
     this._element.classList.remove("popup_opened");
-    document.removeEventListener("keydown", this._handleEscClose);
+    document.removeEventListener("keydown", this._handleEscButtonBound);
   }
 
   _handleEscClose(evt) {
     if (evt.key === "Escape") {
-      //this.close().bind(); --------------- пофиксить!
+      console.log("Hi");
+      console.log(this);
+      this.close();
     }
   }
 
