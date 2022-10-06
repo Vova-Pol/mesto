e744b7ff961708151f65539e3a0cf74293fa263a
Vova Pol <ya-chai777@yandex.ru>
2022-10-06 14:00:47 +0300

    refactor: рефактор класса UserInfo

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -1,20 +1,20 @@
 export class UserInfo {
   constructor({ userNameSelector, userOccupationSelector }) {
-    this._name = document.querySelector(userNameSelector);
-    this._occupation = document.querySelector(userOccupationSelector);
+    this._nameElement = document.querySelector(userNameSelector);
+    this._occupationElement = document.querySelector(userOccupationSelector);
   }
 
   getUserInfo() {
     const userData = {
-      name: this._name.textContent,
-      occupation: this._occupation.textContent,
+      name: this._nameElement.textContent,
+      occupation: this._occupationElement.textContent,
     };
 
     return userData;
   }
 
   setUserInfo(newName, newOccupation) {
-    this._name.textContent = newName;
-    this._occupation.textContent = newOccupation;
+    this._nameElement.textContent = newName;
+    this._occupationElement.textContent = newOccupation;
   }
 }
